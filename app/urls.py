a7f1ed7daabdc7ed7c15218227a9ed08f251e49b
Mohamed Abdelmajid Boutahar <mohamedabdelmajidb@gmail.com>
2025-06-04 20:45:57 +0100

    Create product-detail view and template to display individual product information

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home),
-    path("category/<slug:val>", views.CategoryView.as_view(), name="category")
+    path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
+    path("category-title/<val>", views.CategoryTitle.as_view(), name="category-title"),
+    path("product-detail/<int:pk>", views.ProductDeatil.as_view(), name="product-detail")
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
